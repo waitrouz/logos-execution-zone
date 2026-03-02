6000d61bf240d5b49d5cb0645f7ebe58de29c1c7
jonesmarvin8 <83104039+jonesmarvin8@users.noreply.github.com>
2026-03-02 16:27:11 -0500

    artifacts update

--- test_program_methods/guest/src/bin/nonce_changer.rs
+++ test_program_methods/guest/src/bin/nonce_changer.rs
@@ -11,7 +11,7 @@ fn main() {
     };
 
     let account_pre = &pre.account;
-    let account_post = account_pre.clone();
+    let mut account_post = account_pre.clone();
     account_post.nonce.public_account_nonce_increment();
 
     write_nssa_outputs(
